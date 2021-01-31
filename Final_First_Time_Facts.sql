45adfc4dc3f06296d4e0c0945e9fe01b0819e3dd
ghazale <ghazalze@yahoo.com>
2021-01-31 10:36:46 +0330

    final first time is added

--- Final_First_Time_Facts.sql
+++ Final_First_Time_Facts.sql
@@ -51,5 +51,58 @@ end
 
 exec U_First_Time_Fill_User_rating_fact;
 
---************************************************************
+--*******************************************************************************
+
+
+CREATE or Alter PROCEDURE U_First_Time_Fill_U_Fact_Comments @first_day_v Date,@today Date 
+as
+begin
+	truncate table U_Fact_Comments;
+	declare @passing Date;
+	declare @timekey nvarchar(255);
+	set @passing = @first_day_v;
+	while @today> @passing
+	begin
+
+		if (not exists (select * from DataWarehouse.dbo.[S_Dim_Date] where DataWarehouse.dbo.[S_Dim_Date].FullDateAlternateKey = @passing))
+		begin
+			set @passing=dateadd(day,1,@passing);
+		end
+
+		else 
+		begin
+			insert into DataWarehouse.dbo.U_Fact_Comments_Temp([user_id],course_id,course_key,time_key,comment_id,comment_text) 
+			select staging_area.dbo.Comment.[user_id] ,staging_area.dbo.Comment.course_id , 
+			(select course_key from DataWarehouse.dbo.S_Dim_Course where DataWarehouse.dbo.S_Dim_Course.course_id = staging_area.dbo.Comment.course_id ),
+			(select DataWarehouse.dbo.S_Make_TimeKey (staging_area.dbo.Comment.datetime_created)), staging_area.dbo.Comment.comment_id, staging_area.dbo.Comment.comment_text
+			from staging_area.dbo.Comment
+			where convert(date,staging_area.dbo.Comment.datetime_created)= @passing;
+
+			insert into DataWarehouse.dbo. U_Fact_Comments([user_id],course_id,course_key,time_key,comment_id,comment_text) 
+			select [user_id],course_id,course_key,time_key,comment_id,comment_text from DataWarehouse.dbo.U_Fact_Comments_Temp;
+
+			if (select COUNT(*) from DataWarehouse.dbo.U_Fact_Comments_Temp ) > 0
+			begin
+				insert into DataWarehouse.dbo.U_UsersMart_log (number_of_rows,time_when ,full_time ,fact_name ,[action] )
+				values ((select COUNT(*) from DataWarehouse.dbo.U_Fact_Comments_Temp),@passing,GETDATE(),'U_Fact_Comments','insert first time');
+			end
+			truncate table U_Fact_Comments_Temp;
+			set @passing=dateadd(day,1,@passing);
+			
+			
+
+		end
+	end
+end 
+
+
+exec  U_First_Time_Fill_U_Fact_Comments @first_day_v = '1995-01-01', @today='2021-01-31';
+
+select * from U_Fact_Comments
+select * from U_Fact_Comments_Temp
+
+--*************************************************************************************
+
+
+
 
